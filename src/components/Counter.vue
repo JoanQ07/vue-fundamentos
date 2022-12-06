c66a7f1f1d73584bab69e6d2cfebfb3bd204415c
Joan Quintero <jhoanquintero07@hotmail.com>
2022-12-05 22:37:48 -0500

    ok

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>Counter!!</h2>
+  <p>{{counter}}</p>
+
+  <div>
+    {{counter}}
+  </div>
+
+  <div>
+    {{counter}}
+  </div>
+</template>
+
+<script>
+export default {
+   data() {
+    return {
+        counter: 5
+    }
+   },
+}
+</script>
+
+<style>
+
+</style>
